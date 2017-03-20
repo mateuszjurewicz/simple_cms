a82b61a3c85dcf37fbe4b32b7bad04f6493b232d
mateusz <mateusz.jurewicz@gmail.com>
2017-03-20 21:58:42 +0100

    Initial demo CMS commit for Idego

---
+++ cms/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Company(models.Model):
+    creator = models.ForeignKey('auth.User')
+    owner = models.CharField(max_length =50)
+    name = models.CharField(max_length=50)
+    stock_price = models.FloatField()
+
+    def add(self):
+        self.save()
+
+    def __str__(self):
+        rep = str(self.name) + ' ' + str(self.stock_price) + '$'
+        return rep
