8d0100aa8b5e284c65e7e28fec324323856a61d2
mateusz <mateusz.jurewicz@gmail.com>
2017-03-21 21:28:41 +0100

    Template main_list now shows a table of Company instances

--- cms/views.py
+++ cms/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Company
 
 # Create your views here.
 def main_list(request):
-    return render(request, 'cms/main_list.html', {})
+    # grab all added companies in alphabetical order
+    companies = Company.objects.all().order_by('name')
+    return render(request, 'cms/main_list.html', {'companies' : companies})
