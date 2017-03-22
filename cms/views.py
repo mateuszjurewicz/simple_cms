6391768940da3a4da85bd019cfaf6d5774a35c0b
mateusz <mateusz.jurewicz@gmail.com>
2017-03-22 22:05:13 +0100

    After total failure, redid everything

--- cms/views.py
+++ cms/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Company
 
 # Create your views here.
 def main_list(request):
-    # grab all added companies in alphabetical order
-    companies = Company.objects.all().order_by('name')
-    return render(request, 'cms/main_list.html', {'companies' : companies})
+    if request.user.is_authenticated():
+        # grab all added companies in alphabetical order
+        companies = Company.objects.all().order_by('name')
+        return render(request, 'cms/main_list.html', {'companies': companies})
+    else:
+        return render(request, 'registration2/login.html')
