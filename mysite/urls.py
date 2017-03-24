35fc24c77027b9b9866b12b8c526ae282c19a1b3
mateusz <mateusz.jurewicz@gmail.com>
2017-03-24 21:21:01 +0100

    Re-adding registration

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,13 +15,21 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-# this is for the login & logout
+# for login and logout
 from django.contrib.auth import views as auth_views
+# for user registration
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^home/', include('cms.urls'), name='home'),
-    # here comes the login/logout views, order matters
+    # register user
+    url('^register/', CreateView.as_view(
+            template_name='registration/register.html',
+            form_class=UserCreationForm,
+            success_url='/'), name='register'),
+    # login and logout
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'', auth_views.login, name='login'),
 ]
