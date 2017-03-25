ed1d9be2e173246b061db7f5a36c59908bc33d93
mateusz <mateusz.jurewicz@gmail.com>
2017-03-25 12:36:09 +0100

    Adding users from main list

--- cms/forms.py
+++ cms/forms.py
@@ -1,8 +1,27 @@
 from django import forms
 from .models import Company
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
+
+# for adding new companies from main list
 class CompanyForm(forms.ModelForm):
-
     class Meta:
         model = Company
         fields = ('owner', 'name', 'stock_price')
+
+
+# for adding new users from main list (as opposed to registering when not logged in)
+class MyUserForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('username',)
+
+    def save(self, commit=True):
+        user = super(MyUserForm, self).save(commit=False)
+
+        if commit:
+            user.save()
+
+        return user
--- cms/views.py
+++ cms/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Company
 from .forms import CompanyForm
+from .forms import MyUserForm
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
@@ -11,6 +12,7 @@ def main_list(request):
     companies = Company.objects.all().order_by('name')
     users = User.objects.all().order_by('username')
     return render(request, 'cms/main_list.html', {'companies': companies, 'users': users})
+
 
 @login_required
 def add_new(request):
@@ -24,3 +26,16 @@ def add_new(request):
     else:
         form = CompanyForm()
     return render(request, 'cms/company_add.html', {'form': form})
+
+
+@login_required
+def add_user(request):
+    if request.method == "POST":
+        form = MyUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            return redirect('main_list')
+    else:
+        form = MyUserForm()
+    return render(request, 'cms/user_add.html', {'form': form})
